caedb178a31272eef495a4776178ab0ff22fb942
imxcai <imxcai@gmail.com>
2020-05-20 21:33:43 +0800

    use list(range())

--- Chapter_1/odds.py
+++ Chapter_1/odds.py
@@ -5,7 +5,7 @@ import time
 odds = [ 1, 3, 5, 7, 9, 11, 13, 15, 17, 19,
         21, 23, 25, 27, 29, 31, 33, 35, 37,
         39, 41, 43, 45, 47, 49, 51, 53, 55,
-        57, 59 ]
+        57, 59 ] #can use odds = list(range(1,60,2))
 
 right_this_minute = datetime.today().minute
 
